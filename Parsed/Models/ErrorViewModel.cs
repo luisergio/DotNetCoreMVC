ba4696948a921ce03dcfa5d7e5d08568cb436290
Luis Sérgio <luis.correa@iteris.com.br>
2018-10-17 13:42:17 -0300

    Primeiro commit. Projeto Web DotNet Core 2.0 MVC gerado pelo Visual Studio 2017

---
+++ Parsed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Parsed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
